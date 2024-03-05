062964737c69d0e0650ce2c453e1d80af475161a
luqi09 <794876939@qq.com>
2024-03-05 19:24:01 +0800

    feat: 支持路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./', import.meta.url)),
+      '@src': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
