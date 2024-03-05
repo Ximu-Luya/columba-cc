cb8d7ccee61b700b48da513c2f61e3f0b6bb41ac
luqi09 <794876939@qq.com>
2024-03-05 15:39:34 +0800

    chore: 初始化仓库提交

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+// import { darkTheme, NConfigProvider } from 'naive-ui'
+</script>
+
+<template>
+  <!-- 默认使用暗色模式组件 -->
+  <!-- <NConfigProvider :theme="darkTheme">
+    
+  </NConfigProvider> -->
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/game',
+      name: 'game',
+      // 路由级别的代码拆分
+      // 这将为该路由生成一个单独的块（About.[hash].js）
+      // 当访问该路由时，该块将被延迟加载。
+      component: () => import('../views/ProductList.vue')
+    }
+  ]
+})
+
+export default router
